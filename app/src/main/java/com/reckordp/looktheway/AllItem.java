9bae9b62b8fa44279b0f0be7efbdcf53c3fb043c
Reckordp <rmuhammad134@gmail.com>
2022-04-21 10:18:16 +0700

    Add mode

--- app/src/main/java/com/reckordp/looktheway/AllItem.java
+++ app/src/main/java/com/reckordp/looktheway/AllItem.java
@@ -8,51 +8,18 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link AllItem#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class AllItem extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    public static int MODE_CONFIGURATION = 0;
+    public static int MODE_SELECTION = 1;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public AllItem() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment AllItem.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static AllItem newInstance(String param1, String param2) {
-        AllItem fragment = new AllItem();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    int clickMode;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+        clickMode = MODE_CONFIGURATION;
     }
 
     @Override
@@ -60,5 +27,9 @@ public class AllItem extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_all_item, container, false);
+    }
+
+    public void setMode(int mode) {
+        clickMode = mode;
     }
 }
