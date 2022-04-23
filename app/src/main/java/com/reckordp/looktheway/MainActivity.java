e6a3d845a878f44414e60f7ed93aa3d9ded30a39
Reckordp <rmuhammad134@gmail.com>
2022-04-24 06:08:05 +0700

    Add more flag to launcher

--- app/src/main/java/com/reckordp/looktheway/MainActivity.java
+++ app/src/main/java/com/reckordp/looktheway/MainActivity.java
@@ -2,6 +2,7 @@ package com.reckordp.looktheway;
 
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts.StartActivityForResult;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentTransaction;
 
@@ -27,16 +28,25 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        findViewById(R.id.tambah).setOnClickListener(view -> {
-            Intent intent = new Intent(this, ItemConfiguration.class);
-            intent.putExtra(ItemConfiguration.CONFIGURATION_MODE,
-                    ItemConfiguration.CONFIGURATION_MODE_BARU);
-            itemDetailLauncher.launch(intent);
-        });
-
         AllItem allItemFragment = new AllItem();
+        findViewById(R.id.tambah).setOnClickListener(view -> layangkanDetail(null));
+        allItemFragment.setOnItemClick(this::layangkanDetail);
+
         FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
         ft.add(R.id.lembar, allItemFragment);
         ft.commit();
+    }
+
+    private void layangkanDetail(@Nullable ItemDetail item) {
+        Intent intent = new Intent(this, ItemConfiguration.class);
+
+        if (item == null) {
+            intent.putExtra(ItemConfiguration.CONFIGURATION_MODE, ItemConfiguration.CONFIGURATION_MODE_BARU);
+        } else {
+            intent.putExtra(ItemConfiguration.CONFIGURATION_MODE, ItemConfiguration.CONFIGURATION_MODE_ADA);
+            intent.putExtra(ItemConfiguration.CONFIGURATION_MODE_ADA_ITEM, item);
+        }
+
+        itemDetailLauncher.launch(intent);
     }
 }
